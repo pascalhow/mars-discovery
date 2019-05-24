4672a586b9835c21115cdff2f1b69fb6a5829aed
pascalhow <pascal.how@mydrivesolutions.com>
2019-05-24 17:17:22 +0100

    Handle loading, success and error state

--- app/src/main/java/com/pascalhow/marsdiscovery/activities/mars/MainActivity.kt
+++ app/src/main/java/com/pascalhow/marsdiscovery/activities/mars/MainActivity.kt
@@ -2,18 +2,23 @@ package com.pascalhow.marsdiscovery.activities.mars
 
 import android.arch.lifecycle.Observer
 import android.arch.lifecycle.ViewModelProviders
+import android.graphics.PorterDuff
 import android.os.Bundle
+import android.support.v4.content.ContextCompat
 import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.GridLayoutManager
+import android.util.Log
+import android.view.View
 import com.pascalhow.marsdiscovery.R
 import com.pascalhow.marsdiscovery.data.model.MarsFootage
 import com.pascalhow.marsdiscovery.data.repo.MarsDataStoreFactory
 import com.pascalhow.marsdiscovery.data.repo.MarsRepository
-import com.pascalhow.marsdiscovery.data.resource.Resource
 import com.pascalhow.marsdiscovery.data.resource.ResourceState
 import com.pascalhow.marsdiscovery.utils.NetworkStatusProvider
 import com.pascalhow.marsdiscovery.utils.SchedulersProvider
 import kotlinx.android.synthetic.main.activity_main.mars_footage_recycler_view as recyclerView
+import kotlinx.android.synthetic.main.activity_main.progress_bar as progressBar
+import kotlinx.android.synthetic.main.activity_main.sad_icon as sadIcon
 
 class MainActivity : AppCompatActivity() {
 
@@ -22,6 +27,11 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        progressBar.indeterminateDrawable.setColorFilter(
+            ContextCompat.getColor(this, R.color.colorPrimary)
+            , PorterDuff.Mode.SRC_IN
+        )
 
         val networkStatusProvider = NetworkStatusProvider(applicationContext)
         val repository = MarsRepository(
@@ -41,10 +51,44 @@ class MainActivity : AppCompatActivity() {
     override fun onStart() {
         super.onStart()
         viewModel.getMarsFootageListLiveData().observe(this, Observer { marsFootageList ->
-            marsFootageList?.data?.let {
-                populateMarsFootageList(it)
+            marsFootageList?.let {
+                handleDataState(it.status, it.data, it.message)
             }
         })
+    }
+
+    private fun handleDataState(resourceState: ResourceState, data: List<MarsFootage>?, message: String?) {
+        when (resourceState) {
+            ResourceState.Success -> setUpScreenForSuccess(data)
+            ResourceState.Loading -> setUpScreenForLoading()
+            ResourceState.Error -> setUpScreenForError(message)
+        }
+    }
+
+    private fun setUpScreenForSuccess(data: List<MarsFootage>?) {
+        progressBar.visibility = View.GONE
+
+        if (data != null && data.isNotEmpty()) {
+            sadIcon.visibility = View.GONE
+            populateMarsFootageList(data)
+            recyclerView.visibility = View.VISIBLE
+        } else {
+            recyclerView.visibility = View.GONE
+            sadIcon.visibility = View.VISIBLE
+        }
+    }
+
+    private fun setUpScreenForLoading() {
+        recyclerView.visibility = View.GONE
+        progressBar.visibility = View.VISIBLE
+        sadIcon.visibility = View.GONE
+    }
+
+    private fun setUpScreenForError(message: String?) {
+        recyclerView.visibility = View.GONE
+        progressBar.visibility = View.GONE
+        sadIcon.visibility = View.VISIBLE
+        Log.e("Error loading data", message)
     }
 
     override fun onDestroy() {
